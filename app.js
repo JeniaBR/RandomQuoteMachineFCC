fdb20767768f009d0b6391964c4821fd64380749
Jenia Brook <jeniabrook@gmail.com>
2016-12-04 21:21:15 +0200

    Added Colors and animation Added new font Added Twitter functionality

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+var colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
+
+function setNewColors() {
+    var color = Math.floor(Math.random() * colors.length);
+
+    $("html body").animate({
+        backgroundColor: colors[color],
+        color: colors[color]
+    }, 500);
+
+    $(".btn").animate({
+        backgroundColor: colors[color]
+    }, 500);
+}
+
 function getQuote(){
     
     $.ajax({
@@ -6,18 +21,31 @@ function getQuote(){
             format: 'json'
               },
         success: function (data) {
-            $('#quote-text').text(data.quote);
-            $('#quote-author-text').text(data.author);
+            quote = data.quote;
+            author = data.author;
+            $('#quote').text(quote);
+            $('#quote-author-text').text('-' + author);
         }
     });
+
+    setNewColors();
     }
+
+function tweetOut(){
+    window.open('https://twitter.com/intent/tweet?text=' + '"' + quote + '" ' + author);
+}
 
 
 
 $(document).ready(function(){
-    $('#quote-text').append("This is some quote!!!!!!!");
+    getQuote();
 
     $('#get-quote-btn').click(function(){
         getQuote();
     });
+
+    $('#twitter-btn').click(function(){
+        tweetOut();
+    });
+
 });
